7df3a916f63bbe8abd6f2637725898df0410caee
Jon Gonzales <jon.gonzales@berkeley.edu>
2018-05-02 00:41:27 -0700

    bad checkpoint

--- workspace/src/labs/src/lab7/input_to_PWM.py
+++ workspace/src/labs/src/lab7/input_to_PWM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+
 # Author: Tony Zheng
-# MEC231A BARC Project
+# MEC231A BARC Project 
 
 import rospy
 import time
@@ -46,7 +47,7 @@ def enc_callback(data):
     # compute time elapsed
     tf = time.time()
     dt = tf - t0
-
+    
     # compute speed with second-order, backwards-finite-difference estimate
     v_meas    = r_tire*(3*ang_mean - 4*ang_km1 + ang_km2)/(2*dt)
     # rospy.logwarn("speed = {}".format(v_meas))
@@ -104,10 +105,10 @@ class PID():
     def acc_calculate(self, speed_reference, speed_current):
         self.error = speed_reference - speed_current
         print(self.error)
-
+        
         # Propotional control
         self.P_effect = self.kp*self.error
-
+        
         # Integral control
         self.integrator = self.integrator + self.error
         ## Anti windup
@@ -116,14 +117,14 @@ class PID():
         if self.integrator <= self.integrator_min:
             self.integrator = self.integrator_min
         self.I_effect = self.ki*self.integrator
-
+        
         # Derivative control
         self.derivator = self.error - self.derivator
         self.D_effect = self.kd*self.derivator
         self.derivator = self.error
 
         acc = self.P_effect + self.I_effect + self.D_effect
-
+        
         if acc <= 0:
             acc = 20
         return acc
@@ -132,12 +133,12 @@ class PID():
 def inputToPWM():
     global motor_pwm, servo_pwm, motor_pwm_offset, servo_pwm_offset
     global v_ref, v_meas
-
+    
     # initialize node
     rospy.init_node('inputToPWM', anonymous=True)
-
+    
     global pubname , newECU , subname, move , still_moving
-    newECU = ECU()
+    newECU = ECU() 
     newECU.motor = 1500
     newECU.servo = 1530
     move = False
@@ -156,9 +157,9 @@ def inputToPWM():
     ts          = 1.0 / loop_rate
     rate        = rospy.Rate(loop_rate)
     t0          = time.time()
-
+     
     # Initialize the PID controller
-    longitudinal_control = PID(kp=70, ki=5, kd=1)
+    longitudinal_control = PID(kp=50, ki=1, kd=1)
     maxspeed = 1700
     minspeed = 1300
 
@@ -173,7 +174,7 @@ def inputToPWM():
 
             if (not(move) or not(still_moving)):
                 motor_pwm = 1500
-                servo_pwm = 1530
+                servo_pwm = 1540
 
             # publish information
             pubname.publish( ECU(motor_pwm, servo_pwm) )
