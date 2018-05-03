b062b0e5b3327991587bfa817025a5be0b05181e
Terry <terry_chern@berkeley.edu>
2018-05-03 16:25:40 -0700

    steering dynamics updated lab 4

--- workspace/src/labs/src/lab4/SteeringDynamics.py
+++ workspace/src/labs/src/lab4/SteeringDynamics.py
@@ -36,30 +36,93 @@ def arduino_interface():
     loop_rate   = 50
     dt          = 1.0 / loop_rate
     rate        = rospy.Rate(loop_rate)
-    
+
     time_prev = time.time()
     ecu_pub = Publisher('ecu_pwm', ECU, queue_size = 10)
-    
-    motor_pwm = 1500
+
+    motor_pwm = 1600
     servo_pwm = 1600
     flag = 0
-
+    x0 = 12
+    x1 = x0 + 1
+    x2 = x1 + 2
+    x3 = x2 + 1
+    x4 = x3 + 2
+    x5 = x4 + 1
+    x6 = x5 + 2
+    x7 = x6 + 1
+    x8 = x7 + 7
+    x9 = x8 + 1
+    x10 = x9 + 7
+    x11 = x10 + 1
+    x12 = x11 + 2
+    x13 = x12 + 1
+    x14 = x13 + 2
+    x15 = x14 + 1
+    x16 = x15 + 2
+    x17 = x16 + 1
+    x18 = x17 + 8
     while not rospy.is_shutdown():
-        
-        if time.time()-time_prev>=12:
-            servo_pwm = 1600; #reset to straight ahead
-            ecu_cmd = ECU(motor_pwm, servo_pwm)
-            ecu_pub.publish(ecu_cmd)
-            break
-        elif time.time()-time_prev >=7:
-            servo_pwm = 1625; #send new steering angle command: [1450, 1500, 1575, 1625, 1700,1750]
-
+        if time.time() - time_prev < x0:
+            servo_pwm = 1530
+        elif time.time() - time_prev < x1: #1
+            servo_pwm = 1650
+        elif time.time() - time_prev < x2: #2
+            servo_pwm = 1530
+        elif time.time() - time_prev < x3: #3
+            servo_pwm = 1670
+        elif time.time() - time_prev < x4: #4
+            servo_pwm = 1530
+        elif time.time() - time_prev < x5: #5
+            servo_pwm = 1670
+        elif time.time() - time_prev < x6: #6
+            servo_pwm = 1530
+        elif time.time() - time_prev < x7: #7
+            servo_pwm = 1640
+        elif time.time() - time_prev < x8: #8
+            servo_pwm = 1530
+        elif time.time() - time_prev < x9: #9
+            servo_pwm = 1680
+        elif time.time() - time_prev < x10: #10
+            servo_pwm = 1530
+        elif time.time() - time_prev < x11: #11
+            servo_pwm = 1630
+        elif time.time() - time_prev < x12: #12
+            servo_pwm = 1530
+        elif time.time() - time_prev < x13: #13
+            servo_pwm = 1650
+        elif time.time() - time_prev < x14: #14
+            servo_pwm = 1530
+        elif time.time() - time_prev < x15: #15
+            servo_pwm = 1650
+        elif time.time() - time_prev < x16: #16
+            servo_pwm = 1530
+        elif time.time() - time_prev < x17: #17
+            servo_pwm = 1650
+        elif time.time() - time_prev < x18: #18
+            servo_pwm = 1580
 
         ecu_cmd = ECU(motor_pwm, servo_pwm)
         ecu_pub.publish(ecu_cmd)
-	
-        # wait
+
+        #wait
         rate.sleep()
+    # while not rospy.is_shutdown():
+    #
+    #     if time.time()-time_prev>=12:
+    #         servo_pwm = 1600; #reset to straight ahead
+    #         ecu_cmd = ECU(motor_pwm, servo_pwm)
+    #         ecu_pub.publish(ecu_cmd)
+    #         break
+    #     elif time.time()-time_prev >=7:
+    #         servo_pwm = 1625; #send new steering angle command: [1450, 1500, 1575, 1625, 1700,1750]
+    #
+    #
+    #     ecu_cmd = ECU(motor_pwm, servo_pwm)
+    #     ecu_pub.publish(ecu_cmd)
+    #
+    #     # wait
+    #     rate.sleep()
 
 #############################################################
 if __name__ == '__main__':
