7d3bd6843d2b452b246fc875a71b50bb848938a3
Terry <terry_chern@berkeley.edu>
2018-05-02 20:49:14 -0700

    interval and j changed

--- workspace/src/labs/src/lab7/LQR_Lane_Keeping.py
+++ workspace/src/labs/src/lab7/LQR_Lane_Keeping.py
@@ -457,7 +457,7 @@ class image_processing_node():
                 dt = self.avg2#self.dt
                 lr = 0.15
                 lf = 0.15
-                j = 2 # 4 
+                j = 4 # 4
                 interval = 4 # 1
                 x_ref_for_radius = [x_ref[j+interval],x_ref[j+interval*2]]
                 y_ref_for_radius = [y_ref[j+interval],y_ref[j+interval*2]]
